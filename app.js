1ae6ad951fbab95a0fcddab7e17053bd08ba3bd8
Steven Underwood <stevenunderwood2@hotmail.com>
2023-05-15 13:04:18 -0500

    shells of index.html style.css and app.js

---
+++ app.js
@@ -0,0 +1,12 @@
+// calculator ideas;
+// needs:
+//  screen
+//  buttons (+, -, /, *)
+// inputs (numbers) (0-9)
+// decimal options
+// ~20 operations and numbers
+// 1 for on/off
+// 1 for clear and reset
+// equals to solve problem
+// functions to solve for
+// 
